9835295e013e70f2486137edcccc5712d42240ec
dvsnayak <yello.davis@gmail.com>
2024-02-14 12:19:31 +0530

    Future Added

---
+++ JavaMultithreading/src/com/davis/piersqure/executors/future/FuturePractice.java
@@ -0,0 +1,25 @@
+package com.davis.piersqure.executors.future;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class FuturePractice {
+
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+
+		Callable<String> task = () -> {
+			return "Returning future Object";
+		};
+
+		Future<String> future = executor.submit(task);
+
+		// Shutdown the executor gracefully
+		executor.shutdown();
+		executor.awaitTermination(10, TimeUnit.SECONDS);
+	}
+
+}
